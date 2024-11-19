ba7500c902fbf42e0d1aa39840d6227b5d79770d
Shaan Yadav <shaanyworkspace@gmail.com>
2024-11-19 19:27:40 +0530

    Starts here

---
+++ Hotel_Management_System/hotelManagementSystem.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.sql.DriverManager;//This is a class
+import java.sql.SQLException; //also a class
+
+//All are interfaces
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+public class hotelManagementSystem {
+    //we are making all private -> not to give access to anyone else outsider
+    private static void reserveRoom(){}
+    private static void deleteReservation(){}
+    private static void viewReserve(){}
+    private static void getRoomNo(){}
+    private static void updateRoomNo(){}
+    private static void updateMobileNo(){}
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
